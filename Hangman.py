a0c86b07030a8de0da15eaf56cb9575e4d909514
AlliysonBonner <alliyson.bonner@gmail.com>
2022-12-12 20:07:04 -0600

    Add doc string and comments

--- Hangman.py
+++ Hangman.py
@@ -16,6 +16,15 @@ import string
 from words import words
 
 def get_valid_word(words):
+  """Select a word from a list of words that does not contain any space or dashes,
+  randomly. Ensure all letters of the selected word are uppercase.
+  
+  Args:
+  	words(list): A list of words.
+    
+  Returns(str):
+  	A random uppercase word with no dashes or spaces.
+  """
   word = random.choice(words)
   while '-' in word or ' 'in word: 
     word = random.choice(words)
@@ -23,7 +32,11 @@ def get_valid_word(words):
   return word.upper()
 
 def hangman():
-  # TODO: Describe the next 4 lines.
+  """hangman starts and manages a game of hangman."""
+  # Define word, word_letters, alphabet, and used_letters. Word gets words without dashes
+  # or spaces from the list of words. word_letters stores each unique letter in the word 
+  # that has not been guessed. alphabet has each uppercase letter of the alphabet. 
+  # used_letters holds all guessed letters
   word = get_valid_word(words)
   word_letters = set(word) #we only want each letter 1 time
   alphabet = set(string.ascii_uppercase)
@@ -31,40 +44,44 @@ def hangman():
   
   lives = 6
   
-  # Get the user input
   while len(word_letters) > 0 and lives > 0:
-    # TODO: Add comments
-    # TODO: Add comments
+    # Tell the user how many lives there are for the duration of the game and what 
+    # letters have already been used.
     print('You have', lives, 'lives left and you used these letters: ', ' '.join(used_letters))
     
-    # TODO: Add comments
+    # Display the current word to the user, replacing unguessed letters with '-'.
     word_list = [letter if letter in used_letters else '-' for letter in word]
     print('Current word: ', ' '.join(word_list))
     
+  	# Get the user input
     user_letter = input('Guess a letter: ').upper()
-
-    # TODO: Describe the next 4 lines.
+    
+    # Check if user made an appropriate guess. If user's guess is appropiate add it
+    # to used_letters. If user guess is correct remove it from word_letters. 
+    # Otherwise subtract a life
+    # alphabet = {'A', 'B', 'C' ...} 
+    # used_letters = {'C', 'T', 'B'}
+    # alphabet - used_letters = {'A', 'D', 'E' ... 'S', 'U', ...}
     if user_letter in alphabet - used_letters:
       used_letters.add(user_letter)
       if user_letter in word_letters:
           word_letters.remove(user_letter)
-          
       else:
         lives -= 1
 
-    # TODO: Describe the next 2 lines.
+    # Tell user to pick a new letter because they have already used the one they picked.
     elif user_letter in used_letters:
       print('You have already used that character. Please try again.')
 
-    # TODO: Describe the next 2 lines.
+    # Tell user to enter an alphabet charater.
     else:
       print('Ivalid character. Please try again.')
   
+  # Tell user if they won or lost
   if lives == 0:
     print('You died, sorry. The word was', word)
   else:
  	 print('You guessed the word', word, '!!')
-  
-  
+
 if __name__ == "__main__":
   hangman()
