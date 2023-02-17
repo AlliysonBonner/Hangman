780b93012fc7f6d44fde910726aa9226aa6c5223
AlliysonBonner <alliyson.bonner@gmail.com>
2023-02-17 01:05:06 -0600

    Optimize hangman.py, add image and text to readme.md, and add license.md  * Optimized hangman.py to improve code efficiency and added Google-style docstrings and comments for readability and maintainability. * Added an image and text to the README.md file to provide information and improve the user experience. * Added a license.md file to the repository to specify the usage terms and conditions.

--- Hangman.py
+++ Hangman.py
@@ -1,87 +1,68 @@
-# Hangman
-# This is a game for in which one player tries to guess the letters of a word, and
-# failed attempts are recorded by drawing a gallows and someone hanging on it,
-# line by line.
-
-
-# Author: Alliyson Bonner
-# Github: https://github.com/alliysonbonner
-# Linkedin: https://www.linkedin.com/in/alliyson
-# Email: alliyson.bonner@gmail.com
-# Date: December 09 2022
-
-# import libraries
 import random
 import string
 from words import words
 
 def get_valid_word(words):
-  """Select a word from a list of words that does not contain any space or dashes,
-  randomly. Ensure all letters of the selected word are uppercase.
-  
-  Args:
-  	words(list): A list of words.
-    
-  Returns(str):
-  	A random uppercase word with no dashes or spaces.
-  """
-  word = random.choice(words)
-  while '-' in word or ' 'in word: 
+    """
+    Select a word from a list of words that does not contain any spaces or dashes, randomly. Ensure all letters of
+    the selected word are uppercase.
+
+    Args:
+        words (list): A list of words.
+
+    Returns:
+        str: A random uppercase word with no dashes or spaces.
+    """
     word = random.choice(words)
-    
-  return word.upper()
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+
 
 def hangman():
-  """hangman starts and manages a game of hangman."""
-  # Define word, word_letters, alphabet, and used_letters. Word gets words without dashes
-  # or spaces from the list of words. word_letters stores each unique letter in the word 
-  # that has not been guessed. alphabet has each uppercase letter of the alphabet. 
-  # used_letters holds all guessed letters
-  word = get_valid_word(words)
-  word_letters = set(word) #we only want each letter 1 time
-  alphabet = set(string.ascii_uppercase)
-  used_letters = set()
-  
-  lives = 6
-  
-  while len(word_letters) > 0 and lives > 0:
-    # Tell the user how many lives there are for the duration of the game and what 
-    # letters have already been used.
-    print('You have', lives, 'lives left and you used these letters: ', ' '.join(used_letters))
-    
-    # Display the current word to the user, replacing unguessed letters with '-'.
-    word_list = [letter if letter in used_letters else '-' for letter in word]
-    print('Current word: ', ' '.join(word_list))
-    
-  	# Get the user input
-    user_letter = input('Guess a letter: ').upper()
-    
-    # Check if user made an appropriate guess. If user's guess is appropiate add it
-    # to used_letters. If user guess is correct remove it from word_letters. 
-    # Otherwise subtract a life
-    # alphabet = {'A', 'B', 'C' ...} 
-    # used_letters = {'C', 'T', 'B'}
-    # alphabet - used_letters = {'A', 'D', 'E' ... 'S', 'U', ...}
-    if user_letter in alphabet - used_letters:
-      used_letters.add(user_letter)
-      if user_letter in word_letters:
-          word_letters.remove(user_letter)
-      else:
-        lives -= 1
+    """
+    This function starts and manages a game of hangman.
+    """
+    word = get_valid_word(words)
+    word_letters = set(word)  # A set of the unique letters in the word
+    alphabet = set(string.ascii_uppercase)  # A set of uppercase letters of the alphabet
+    used_letters = set()  # A set of letters the user has guessed
+    lives = 6  # The number of lives the user has
 
-    # Tell user to pick a new letter because they have already used the one they picked.
-    elif user_letter in used_letters:
-      print('You have already used that character. Please try again.')
+    while len(word_letters) > 0 and lives > 0:
+        # Display the number of lives remaining and the letters the user has guessed
+        print(f"You have {lives} lives left and you've used these letters: {' '.join(used_letters)}")
 
-    # Tell user to enter an alphabet charater.
+        # Display the current word to the user, replacing unguessed letters with '-'
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(f"Current word: {' '.join(word_list)}")
+
+        # Get user input
+        user_letter = input("Guess a letter: ").upper()
+
+        if user_letter in alphabet - used_letters:
+            # If the user's guess is valid, add it to used_letters
+            used_letters.add(user_letter)
+
+            if user_letter in word_letters:
+                # If the user's guess is in the word, remove it from word_letters
+                word_letters.remove(user_letter)
+            else:
+                # If the user's guess is not in the word, subtract a life
+                lives -= 1
+        elif user_letter in used_letters:
+            # If the user has already guessed the letter, ask them to pick another one
+            print("You've already used that letter. Please try again.")
+        else:
+            # If the user's guess is not a valid letter, ask them to try again
+            print("Invalid character. Please try again.")
+
+    # The game is over: either the user has won or lost
+    if lives == 0:
+        print(f"You died, sorry. The word was {word}")
     else:
-      print('Ivalid character. Please try again.')
-  
-  # Tell user if they won or lost
-  if lives == 0:
-    print('You died, sorry. The word was', word)
-  else:
- 	 print('You guessed the word', word, '!!')
+        print(f"Congratulations! You guessed the word {word}!!")
+
 
 if __name__ == "__main__":
-  hangman()
+    hangman()
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Hangman
+![A cartoon of two young men locked in an aggressive match of Rock, Paper, Scissors.](https://github.com/AlliysonBonner/Hangman/blob/main/image.png?raw=true)
+Hangman is a word guessing game where you must guess the letters of a secret word. Each incorrect guess results in the drawing of a new part of a gallows and a person hanging from it, line by line. The game is over when the word is guessed correctly or the person is fully drawn.
+
+## Usage
+1. Run the game by running python hangman.py in your terminal or command prompt.
+1. Enter your guess for a letter when prompted, and press enter.
+1. The game will continue until the word is guessed correctly or the person is fully drawn.
+
+## Contributing
+If you'd like to contribute to Hangman, please fork the repository and make your changes. Submit a pull request and the repository owner will review your changes.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/alliysonbonner/Hangman/LICENSE.md) file for details.
+
+## Author
+* Name: Alliyson Bonner
+* Github: https://github.com/alliysonbonner
+* LinkedIn: https://www.linkedin.com/in/alliyson
+* Email: alliyson.bonner@gmail.com
